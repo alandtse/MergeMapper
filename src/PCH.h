54cc3f028e7bd2e83f50ab36862164d11524d5a2
Alan Tse <alandtse@gmail.com>
2023-05-12 01:06:00 -0700

    build: update commonlibng

--- src/PCH.h
+++ src/PCH.h
@@ -1,14 +1,32 @@
 #pragma once
 
+#include <RE/Skyrim.h>
+#include <REL/Relocation.h>
+#include <SKSE/SKSE.h>
+#include <Windows.h>
+
+#include <algorithm>
+#include <any>
+#include <array>
+#include <atomic>
+#include <barrier>
+#include <bit>
+#include <bitset>
 #include <cassert>
 #include <cctype>
 #include <cerrno>
 #include <cfenv>
 #include <cfloat>
+#include <charconv>
+#include <chrono>
 #include <cinttypes>
 #include <climits>
 #include <clocale>
 #include <cmath>
+#include <compare>
+#include <complex>
+#include <concepts>
+#include <condition_variable>
 #include <csetjmp>
 #include <csignal>
 #include <cstdarg>
@@ -21,20 +39,6 @@
 #include <cuchar>
 #include <cwchar>
 #include <cwctype>
-
-#include <algorithm>
-#include <any>
-#include <array>
-#include <atomic>
-#include <barrier>
-#include <bit>
-#include <bitset>
-#include <charconv>
-#include <chrono>
-#include <compare>
-#include <complex>
-#include <concepts>
-#include <condition_variable>
 #include <deque>
 #include <exception>
 #include <execution>
@@ -94,14 +98,6 @@
 #include <variant>
 #include <vector>
 #include <version>
-
-#include <RE/Skyrim.h>
-#include <REL/Relocation.h>
-#include <SKSE/SKSE.h>
-
-#include <Psapi.h>
-#include <ShlObj_core.h>
-#include <Windows.h>
 #undef cdecl  // Workaround for Clang 14 CMake configure error.
 
 #include <spdlog/sinks/basic_file_sink.h>
